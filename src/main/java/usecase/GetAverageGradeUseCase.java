4f0ad24013efd6cc4a3413452e68ac98333154c8
sbocoun <sbocoun@gmail.com>
2024-09-30 15:30:50 -0400

    Implemented the getAverageGrade method for GetAverageGradeUseCase

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -23,12 +23,18 @@ public final class GetAverageGradeUseCase {
         // Call the API to get usernames of all your team members
         float sum = 0;
         int count = 0;
-        // TODO Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
+        // Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
-        // TODO Task 3a: Complete the logic of calculating the average course grade for
-        //              your team members. Hint: the getGrades method might be useful.
-
+        // Task 3a: Complete the logic of calculating the average course grade for
+        // your team members. Hint: the getGrades method might be useful.w
+        String[] students = team.getMembers();
+        for (String student : students) {
+            Grade gradeobj = gradeDataBase.getGrade(student, course);
+            int grade = gradeobj.getGrade();
+            sum += grade;
+            count++;
+        }
         if (count == 0) {
             return 0;
         }
